7787112fce7135dfe8df249c7fa303f6ed315671
Swapnil Mishra <swapnilmishra.anu@gmail.com>
2016-12-16 09:42:34 +1100

    Adding exponential kernel and it's corresponding tutorial

--- code/rscripts/marked_hawkes.R
+++ code/rscripts/marked_hawkes.R
@@ -1,7 +1,17 @@
 ## source simulation file required to gt simulating algorithm
 source('rscripts/simulation.R')
 
-#' Triggering Kernel Function
+#' Tirggering Kernel Function dummy
+#' 
+#' Just to pass the kernel type
+kernelFct <- function(event, t, K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c = 0.001, theta = 0.2, inclusive = T, 
+                      kernel.type='PL') {
+  switch(kernel.type,
+         PL = .kernelFct.PL(event, t, K = K, alpha = alpha, beta = beta, mmin = mmin, c = c, theta = theta, inclusive = inclusive),
+         EXP = .kernelFct.EXP(event, t, K = K, alpha = alpha, beta = beta, mmin = mmin, theta = theta, inclusive = inclusive))
+}
+
+#' Triggering Kernel Function for Power Law
 #'
 #'  calculates the influence of a given event at the givent time the event is a 2
 #' elements list (mi, ti). Inclusive parameter means that if an event is present
@@ -16,7 +26,7 @@ source('rscripts/simulation.R')
 #' @aparma c the cutoff parameter in social kernel
 #' @param theta th powerlaw exponent of social kernel
 #' @param inclusive whether to consider event at time t for calculation 
-kernelFct<- function(event, t, K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, 
+.kernelFct.PL<- function(event, t, K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, 
                      c = 0.001, theta = 0.2, inclusive = T) {
   # the event has 2 components: (magnitude_i, time_i)
   mat_event = matrix(unlist(event), ncol = 2, byrow = F)
@@ -42,6 +52,34 @@ kernelFct<- function(event, t, K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1,
   return (val)
 }
 
+#' Triggering Kernel Function for Exponential
+#' 
+#' Onlt thing that changes from power law is there is no c parameter here
+.kernelFct.EXP <- function(event, t, K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, theta = 0.2, inclusive = T) {
+  # the event has 2 components: (magnitude_i, time_i)
+  mat_event = matrix(unlist(event), ncol = 2, byrow = F)
+  mi = mat_event[,1]
+  ti = mat_event[,2]
+  
+  # f(p_j) part - virality of a video. Constant for a given video
+  fun_f <- K
+  
+  # ro(m_i) part - the influence of the user of the event
+  fun_ro <- (mi / mmin) ^ beta
+  
+  # psi(t, ti) part - the decaying / relaxation kernel
+  fun_psi <- theta * (exp(-theta * (t - ti)))
+  
+  val = fun_f * fun_ro * fun_psi
+  val[t<ti] = 0
+  val[mi<mmin] = 0
+  if (!inclusive) {
+    val[t == ti] = 0
+    val[mi == mmin] = 0
+  }
+  
+  return (val)
+}
 
 #' Conditional intensity function
 #' 
@@ -70,7 +108,7 @@ CIF = function(x, history, ...) {
 #' @param Tmax - maximum time of simulation.
 #' @param filename - file to which save the CSV file with the simulation.
 generate_Hawkes_event_series <- function(K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c = 0.001, theta = 0.2,
-                                         M=10000, Tmax = 10, filename = NULL, history_init = NULL) {
+                                         M=10000, Tmax = 10, filename = NULL, history_init = NULL, kernel.type = 'PL') {
   saveInterval = 1000
   history <- NULL
   
@@ -113,14 +151,14 @@ generate_Hawkes_event_series <- function(K = 0.024, alpha = 2.016, beta = 0.5, m
   while(history[nrow(history), "time"] <= Tmax){
     # generate the time of the next event, based on the previous events
     t.lambda.max <- t
-    intensityMax <- CIF(x = t.lambda.max, history = history, K, alpha, beta, mmin, c, theta)
+    intensityMax <- CIF(x = t.lambda.max, history = history, K, alpha, beta, mmin, c, theta, kernel.type=kernel.type)
     
     # I need this other function, because in the Poisson generation, the time index starts at 0
     # and I need to translate it at the end of my time.
     subsCIF <- function(x, ...) CIF(x + t, ...) 
     # the Tmax = NULL is a dirty trick to convince R to accept my additional parameters
     x <- rnhpoisson(Tmax = NULL, Nmax = 1, LambdaMax = intensityMax, FUN = subsCIF, 
-                    history, K, alpha, beta, mmin, c, theta) ## these parameters get transmitted using ...
+                    history, K, alpha, beta, mmin, c, theta, kernel.type=kernel.type) ## these parameters get transmitted using ...
     t = t + x
     
     # generate the influence of the next event, by sampling the powerlaw distribution of the #retweets
@@ -148,7 +186,17 @@ generate_Hawkes_event_series <- function(K = 0.024, alpha = 2.016, beta = 0.5, m
   return(history)
 }
 
-#' Calculate Branching factor
+#' Calculate Branching factor 
+#' 
+#' Dummy to pass kernel type to get branching factor
+getBranchingFactor <- function(K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c = 0.001, 
+                               theta = 0.2, kernel.type = 'PL') {
+  switch(kernel.type,
+         PL = .getBranchingFactor.PL(K = K, alpha = alpha, beta = beta, mmin = mmin, c = c, theta = theta),
+         EXP = .getBranchingFactor.EXP(K = K, alpha = alpha, beta = beta, mmin = mmin, theta = theta))
+}
+
+#' Calculate Branching factor for Power Law Kernel
 #' 
 #' calculates the average number of events generated by an event
 #' in our Social kernel
@@ -160,7 +208,7 @@ generate_Hawkes_event_series <- function(K = 0.024, alpha = 2.016, beta = 0.5, m
 #' @aparma c the cutoff parameter in social kernel
 #' @param theta th powerlaw exponent of social kernel
 #' @return brnaching factor forthe social kernel with parameters passed
-getBranchingFactor<- function(K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c = 0.001, theta = 0.2) {
+.getBranchingFactor.PL<- function(K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c = 0.001, theta = 0.2) {
   
   if (beta >= (alpha - 1) ) {
     warning("The closed expression calculated by this function does NOT hold for beta >= alpha - 1")
@@ -181,9 +229,39 @@ getBranchingFactor<- function(K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c 
   return (n) 
 }
 
-#' Gets total tweets for our Social Kernel with current history
+#' Calculate Branching factor for Exponential Kernel
 #' 
-getTotalEvents<- function(history, bigT, K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c = 0.001, 
+#' calculates the average number of events generated by an event
+.getBranchingFactor.EXP <- function(K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c = 0.001, theta = 0.2) {
+  
+  if (beta >= (alpha - 1) ) {
+    warning("The closed expression calculated by this function does NOT hold for beta >= alpha - 1")
+    
+    return (Inf)
+  }
+  
+  if (theta <= 0 ) {
+    warning(sprintf("The closed expression calculated by this function does NOT hold for theta <= 0 (K=%.4f, beta=%.2f, theta=%.2f)", K, beta, theta))
+    
+    return (Inf)
+  }
+  
+  n = (K * (alpha - 1)) / (alpha - 1 - beta)
+  return (n) 
+}
+
+#' Dummy Function to estimate the total retweets, use to pass kernel type
+#' 
+getTotalEvents <- function(history, bigT, K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c = 0.001, 
+                           theta = 0.2, kernel.type = 'PL') {
+  switch(kernel.type,
+         PL = .getTotalEvents.PL(history, bigT, K = K, alpha = alpha, beta = beta, mmin = mmin, c = c, theta = theta),
+         EXP = .getTotalEvents.EXP(history, bigT, K = K, alpha = alpha, beta = beta, mmin = mmin, theta = theta))
+}
+
+#' Gets total tweets for our Power Law Social Kernel with current history
+#' 
+.getTotalEvents.PL<- function(history, bigT, K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c = 0.001, 
                                theta = 0.2 ){
   n.star <- getBranchingFactor(K, alpha, beta, mmin, c, theta)
   if (n.star >= 1) {
@@ -197,6 +275,29 @@ getTotalEvents<- function(history, bigT, K = 0.024, alpha = 2.016, beta = 0.5, m
     # calculating the expected size of first level of descendants
     a1 <-  sum(sapply(X = 1:nrow(history), FUN = function(i) {
       val <- (history$magnitude[i]) ^ beta / (theta * ((bigT + c - history$time[i]) ^ theta))
+      return(val)
+    })) * K
+    # calculating the final value
+    total.tweets = round(dim(history)[1] + a1 / (1 - n.star))
+    return (c(total = total.tweets, nstar = n.star, a1 = a1))
+  }
+}
+#' Gets total tweets for a exponential kernel with current history
+#' 
+.getTotalEvents.EXP <- function(history, bigT, K = 0.024, alpha = 2.016, beta = 0.5, mmin = 1, c = 0.001, 
+                                theta = 0.2 ){
+  n.star <- .getBranchingFactor.EXP(K, alpha, beta, mmin, c, theta)
+  if (n.star >= 1) {
+    warning(sprintf("Branching Factor greater than 1, not possible to predict the size(super critical)"))
+    
+    return (c(total = Inf, nstar = n.star, a1 = 'NA'))
+    
+  } else{
+    # getting T from data
+    # bigT <- max(history$time)
+    # calculating the expected size of first level of descendants
+    a1 <-  sum(sapply(X = 1:nrow(history), FUN = function(i) {
+      val <- (history$magnitude[i]) ^ beta / exp((bigT - history$time[i]) * theta)
       return(val)
     })) * K
     # calculating the final value
@@ -224,9 +325,17 @@ lambda <-  function(t, history, params = c(K = 0.024, beta = 0.5, c = 0.001, the
   return(res)
 }
 
+#' A dummy function to calculate the value of the integral of lambda.
+#' use to pass the kernel type
+integrateLambda <- function(kernel.type = 'PL', .numerical.integration = F, ...) {
+  switch(kernel.type,
+         PL = .integrateLambda.PL(...),
+         EXP = .integrateLambda.EXP(...))
+}
+
 #' A function to calculate the value of the integral of lambda for
 #' Social Kernel
-integrateLambda<- function(lower, upper, history, params, mmin = 1) {
+.integrateLambda.PL<- function(lower, upper, history, params, mmin = 1) {
   if (length(params) == 4)
     names(params) <- c("K", "beta", "c", "theta")
   params <- as.list(unlist(params))
@@ -243,13 +352,29 @@ integrateLambda<- function(lower, upper, history, params, mmin = 1) {
   return(res)
 }
 
+#' A function to calculate the value of the integral of lambda for 
+#' Exponential Kernel
+.integrateLambda.EXP <- function(lower, upper, history, params, mmin = 1) {
+  if (length(params) == 4)
+    names(params) <- c("K", "beta", "c", "theta")
+  params <- as.list(unlist(params))
+  
+  ## closed form
+  res <- sum(sapply(X = 1:nrow(history), FUN = function(i) {
+    val <- (history$magnitude[i] / mmin)^params$beta * 
+      (1 - 1 / (exp(params$theta * (upper - history$time[i])))) 
+    return(val)
+  })) * params$K
+  return(res)
+}
+
 #'  calculates the negative log-likelihood. 
 #'  This is given that the optim function minimizes a function. '
 #' Minimizing the -1 * log-likelihood amounts to maximizing the log-likehood. '
 neg.log.likelihood <- function(params, history,
                                lowerBound = c(K = 0, beta = 0, c = 0, theta = 0), 
-                               upperBound = c(K = Inf, beta = Inf, c = Inf, theta = Inf)
-                               ) { 
+                               upperBound = c(K = Inf, beta = Inf, c = Inf, theta = Inf),
+                               kernel.type = 'PL') { 
   
   if (length(params) == 4)
     names(params) <- c("K", "beta", "c", "theta")
@@ -269,12 +394,22 @@ neg.log.likelihood <- function(params, history,
   
   ## get the formula of the log-likelihood * -1.
   ## that in the summation, we eliminated the first event, as there is no background rate in our lambda(t)
-  return(integrateLambda(lower = 0, upper = bigT, history = history, params = params) - 
-           sum(log(lambda(t = history$time[-1], history = history, params = params))) )
+  return(integrateLambda(lower = 0, upper = bigT, history = history, params = params, kernel.type=kernel.type) - 
+           sum(log(lambda(t = history$time[-1], history = history, params = params, kernel.type=kernel.type))) )
 }
 
-#' A function to calculate the dervative in closed form for our Social Kernel
-closedGradient <- function(params, history,
+#' A dummy function to calculate the dervative in closed form.
+#' just used to pass kernel type
+closedGradient <- function(params, history, kernel.type = 'PL',
+                           lowerBound = c(K = 0, beta = 0, c = 0, theta = 0), 
+                           upperBound = c(K = Inf, beta = Inf, c = Inf, theta = Inf)) {
+  switch(kernel.type,
+         PL = .closedGradient.PL(params, history),
+         EXP = .closedGradient.EXP(params, history))
+}
+
+#' A function to calculate the dervative in closed form for our Power Law Social Kernel
+.closedGradient.PL <- function(params, history,
                            lowerBound = c(K = 0, beta = 0, c = 0, theta = 0), 
                            upperBound = c(K = Inf, beta = Inf, c = Inf, theta = Inf)){
   if (length(params) == 4)
@@ -367,26 +502,123 @@ closedGradient <- function(params, history,
   return(-1 * c(derivK, derivBeta, derivC, derivTheta))
 }
 
-#' A function to specify the constraint.
-constraint <- function(params, history,
+#' A function to calculate the dervative in closed form for Exponential Kernel.
+.closedGradient.EXP <- function(params, history){
+  if (length(params) == 4)
+    names(params) <- c("K", "beta", "c", "theta")
+  
+  # variables collection
+  params <- as.list(unlist(params))
+  bigT <- max(history$time)
+  n <- dim(history)[1]
+  k <- params$K
+  beta <- params$beta
+  theta <- params$theta
+  
+  # In all the following calcuaion, res is the part of derivative coming 
+  # from bigLambda part and first is derivative coming from 
+  # summation of log(lambda(ti))
+  
+  #calculating derivative wrt k in closed form
+  res <- sum(sapply(X = 1:nrow(history), FUN = function(i) {
+    val <- (history$magnitude[i])^beta * 
+      (1 - 1 / (exp(theta * (bigT - history$time[i]))))
+    return(val)
+  }))
+  derivK <- ((n-1) / k) - res
+  
+  #calculating dervative wrt beta in closed form
+  res <- sum(sapply(X = 1:nrow(history), FUN = function(i) {
+    val <- (history$magnitude[i]) ^ beta * log(history$magnitude[i]) *
+      (1 - 1 / (exp(theta * (bigT - history$time[i]))))
+    return(val)
+  })) * k
+  
+  # we go only from second row as first row has no history before
+  # (inner sum goes strictly one less) and we do not have a mu(background rate)
+  # numerically (history$magnitude[1:i-1] or (history$time[1:i-1] is zero
+  first <- sum(sapply(X = 2:nrow(history), FUN = function(i) {
+    
+    numerator <- sum(exp(-theta * (history$time[i] - history$time[1:i-1])) * 
+                       log(history$magnitude[1:i-1]) * 
+                       (history$magnitude[1:i-1] ^ beta))
+    
+    denominator <- sum(exp(-theta * (history$time[i] - history$time[1:i-1])) * 
+                         (history$magnitude[1:i-1] ^ beta))
+    return(numerator / denominator)
+  }))
+  
+  derivBeta <- first - res
+  
+  #calculating derivative wrt theta in closed form
+  res <- sum(sapply(X = 1:nrow(history), FUN = function(i) {
+    val <- (history$magnitude[i]) ^ beta * (bigT - history$time[i]) *
+      (exp(-theta * (bigT - history$time[i])))
+    return(val)
+  })) * k
+  
+  first <- sum(sapply(X = 2:nrow(history), FUN = function(i) {
+    
+    numerator <- sum(exp(-theta * (history$time[i] - history$time[1:i-1])) * 
+                       - (history$time[i] - history$time[1:i-1]) * 
+                       (history$magnitude[1:i-1] ^ beta))
+    
+    denominator <- sum(exp(-theta * (history$time[i] - history$time[1:i-1])) * 
+                         (history$magnitude[1:i-1] ^ beta))
+    return(numerator / denominator)
+  }))
+  derivTheta <- ((n-1) / theta) + first - res
+  
+  ## added a -1 because we are minimizing log-likelihood
+  return(-1 * c(derivK, derivBeta,0, derivTheta))
+}
+
+#' A dummy function to specify the constraint, just to pass kernel type
+constraint <- function(params, history, kernel.type = 'PL',
                        lowerBound = c(K = 0, beta = 0, c = 0, theta = 0), 
                        upperBound = c(K = Inf, beta = Inf, c = Inf, theta = Inf)) {
   if (length(params) == 4)
     names(params) <- c("K", "beta", "c", "theta")
   
+  switch(kernel.type,
+         PL = .constraint.PL(params),
+         EXP = .constraint.EXP(params))
+}
+
+#' A function to specify the constraint for Power Law Kernel
+.constraint.PL <- function(params, history,
+                       lowerBound = c(K = 0, beta = 0, c = 0, theta = 0), 
+                       upperBound = c(K = Inf, beta = Inf, c = Inf, theta = Inf)) {
   return( log(params[1]) + log(1.1016) - log(1.016 - params[2]) - 
             log(params[4]) - ( params[4] * log(params[3]) ) )
 }
+# constraint function for Exponential kernel
+.constraint.EXP <- function( params) {
+  return( 1.016*params[1] + params[2] )
+}
 
-#' A function to specify the Jacobian.
-jacobian <- function(params, history,
+#' A dummy function to specify the Jacobian, just to pass kernel type.
+jacobian <- function(params, history, kernel.type = 'PL',
                      lowerBound = c(K = 0, beta = 0, c = 0, theta = 0), 
                      upperBound = c(K = Inf, beta = Inf, c = Inf, theta = Inf)) {
   if (length(params) == 4)
     names(params) <- c("K", "beta", "c", "theta")
   
+  switch(kernel.type,
+         PL = .jacobian.PL(params),
+         EXP = .jacobian.EXP(params))
+}
+
+#' A function to specify the Jacobian for Power Law Kernel
+.jacobian.PL <- function(params, history,
+                     lowerBound = c(K = 0, beta = 0, c = 0, theta = 0), 
+                     upperBound = c(K = Inf, beta = Inf, c = Inf, theta = Inf)) {
   return ( c( 1 / params[1], 1 / (1.016 - params[2]), -params[4] / params[3],
               -(1 / params[4]) - log(params[3]) ) )
+}
+# Jacobian for Exponential kernel
+.jacobian.EXP <- function (params){
+  return (c(1.016, 1, 0, 0))
 }
 
 ## Functions listed here are helper functions to help us
@@ -408,7 +640,7 @@ createStartPoints <- function(seed = NULL){
 
 #' Function to run ipopt
 require(ipoptr) 
-fitParameters <- function(start, history){
+fitParameters <- function(start, history, kernel.type='PL'){
   
   if (length(start) == 4)
     names(start) <- c("K", "beta", "c", "theta")
@@ -418,8 +650,15 @@ fitParameters <- function(start, history){
               #, "file_print_level"=7, output_file="scripts/ipoptr.out"
   )
  
-  constraint_lb <- c(log(.Machine$double.eps))
-  constraint_ub <- c(log(1 - .Machine$double.eps))
+  if (kernel.type == 'PL'){
+    constraint_lb <- c(log(.Machine$double.eps))
+    constraint_ub <- c(log(1 - .Machine$double.eps))
+  } else{
+    constraint_lb <- c(.Machine$double.eps)
+    # it is not kept with machine tolerance because of equality
+    # being satisfied
+    constraint_ub <- c(1.016 - 1e-06)
+  }
   
   res <- ipoptr(x0 = c(K = start$K, beta = start$beta, 
                        c = start$c, theta =start$theta),
@@ -433,7 +672,7 @@ fitParameters <- function(start, history){
                 constraint_lb = constraint_lb, 
                 constraint_ub = constraint_ub,
                 opts = opts, # options for algorithm
-                history = history,
+                history = history, kernel.type = kernel.type,
                 lowerBound = c(K = 0, beta = 0, c = 0, theta = 0),
                 ## these are parameters for the fn and gr functions
                 upperBound = c(K = 1, beta = 1.016, c = Inf, 
